e0a9101845855ae4c43fcad89cc011db26d9833f
CHOI DAE GEON <chleorjs37@gmail.com>
2021-09-13 07:44:56 +0900

    스크롤 애니메이션 추가

--- js/script_main.js
+++ js/script_main.js
@@ -1,8 +1,29 @@
 //1.gnb 마우스오버, 포커스시 밑줄 => span.bar
 $(function () {
     $('.gnb li a').on('mouseenter focus', function () {
-        var left = $(this).position().left;
-        var width = $(this).width();
-        $('span.bar').css({left:`${left}px`,width:`${width}px`,opacity:1})
+        var bar = $(this).position().left;
+        var widNum = $(this).width();
+        $('span.bar').css({ left: `${bar}px`, width: `${widNum}px`, opacity: 1 });
     })
-})
+
+    $('.gnb li a').on('mouseleave', function () {
+        $('span.bar').css({ left: 0, width: 0, opacity: 0 })
+    })
+});
+
+/* circleBox SVG길이 구하기 */
+$(function () {
+    $('.svgAni').find('path').each(function (i, path) {
+        var length = path.getTotalLength();
+        console.log(length);
+    })
+});
+
+
+/* 스크롤 애니메이션  */
+$(function(){
+    $('.animate').scrolla({
+        mobile:true,
+        once:true
+    })
+});
