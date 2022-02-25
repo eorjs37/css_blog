a4f15743e06a6f18495234b6c0c18fbd0132e4a7
chleorjs <chleorjs37@gmail.com>
2022-02-26 08:50:37 +0900

    css 공부 완료

--- js/script_main.js
+++ js/script_main.js
@@ -46,3 +46,15 @@ $(function(){
         }
     }
 });
+
+/**
+ * 햄버거메뉴
+ */
+$(function () {
+    $('.menuOpen button.open').on('click', function () {
+        $('.menuOpen .menuWrap').addClass('on');
+    });
+    $('.menuOpen .menuWrap .close').on('click', function () {
+        $('.menuOpen .menuWrap').removeClass('on');
+    })
+})
